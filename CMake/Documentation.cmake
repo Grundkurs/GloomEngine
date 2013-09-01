d9ed6a4027594becf8f0c4a8e736e440b8c1da0f
Rafbill <rafaelbocquet+github@gmail.com>
2013-09-01 20:52:10 +0200

    Added .gitignore and removed files

--- CMake/Documentation.cmake
+++
@@ -1,7 +0,0 @@
-# --- LeeEngine3/CMake/Documentation.cmake ---
-
-add_custom_target(doc
-  COMMAND doxygen ${PROJECT_SOURCE_DIR}/Doxyfile
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  VERBATIM
-)
--- Projects/GloomEngine/include/Core/Config.h.cmake
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#cmakedefine LEE_PLATFORM_WINDOWS 1
-#cmakedefine LEE_PLATFORM_LINUX 1
-
-#cmakedefine LEE_DEBUG 1
